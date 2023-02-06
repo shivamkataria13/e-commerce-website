2f5e2cf0383f356d9b81ca8f2dccf4ba9e39fa46
Shivam Kataria <114555700+shivamkataria13@users.noreply.github.com>
2023-02-06 21:54:35 +0530

    index page done

---
+++ script.js
@@ -0,0 +1,12 @@
+const navbar = document.querySelector("#navbar")
+const openmenu = document.getElementById("menu")
+const closemenu = document.getElementById("menu-close")
+
+
+openmenu.addEventListener("click", () => {
+    navbar.classList.add("open")
+} )
+
+closemenu.addEventListener("click", () => {
+    navbar.classList.remove("open")
+} )
