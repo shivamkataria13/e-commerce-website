d723d6b125b664e33d047cd1ab59d5fd166a477c
Shivam Kataria <114555700+shivamkataria13@users.noreply.github.com>
2023-02-07 16:36:17 +0530

    shop page and single product page done

--- script.js
+++ script.js
@@ -10,3 +10,24 @@ openmenu.addEventListener("click", () => {
 closemenu.addEventListener("click", () => {
     navbar.classList.remove("open")
 } )
+
+
+
+
+// single product page
+let mainImg = document.getElementById('main-img')
+let smallImg = document.getElementsByClassName('small-img')
+
+
+smallImg[0].onclick = function(){
+    mainImg.src = smallImg[0].src;  
+}
+smallImg[1].onclick = function(){
+    mainImg.src = smallImg[1].src;  
+}
+smallImg[2].onclick = function(){
+    mainImg.src = smallImg[2].src;  
+}
+smallImg[3].onclick = function(){
+    mainImg.src = smallImg[3].src;  
+}
